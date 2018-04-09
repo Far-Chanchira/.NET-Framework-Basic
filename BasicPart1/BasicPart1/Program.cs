fe811bf3586d29218f9925accaa0b09e2c7519bd
chanchira <chanchiray38@gmail.com>
2018-04-09 10:49:49 +0700

    Variable

---
+++ BasicPart1/BasicPart1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 1995;
+            string name = "Chanchira";
+            double points = 3.50;
+            bool status = false;
+            year = year + 543;
+            Console.WriteLine("My name is :" + name);
+            Console.WriteLine("Grade :" + points);
+            Console.WriteLine("Years :" + year);
+            Console.WriteLine("Status :" + status);
+            Console.ReadKey();
+        }
+    }
+}
